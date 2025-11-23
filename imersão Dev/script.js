417c87674aaaab11bb936ae5519f27b1b7fa0aaa
Erick <erickgoud19@gmail.com>
2025-11-23 10:40:16 -0300

    Add files via upload

---
+++ imersão Dev/script.js
@@ -0,0 +1,50 @@
+let cardContainer = document.querySelector(".card-container");
+let campoBusca = document.querySelector("header input");
+let dados = [];
+
+async function iniciarBusca() {
+    if (dados.length === 0) {
+        try {
+            let resposta = await fetch("data.json");
+            dados = await resposta.json();
+        } catch (error) {
+            console.error("Falha ao buscar dados:", error);
+            return;
+        }
+    }
+
+    const termoBusca = campoBusca.value.toLowerCase();
+    const dadosFiltrados = dados.filter(dado =>
+        dado.nome.toLowerCase().includes(termoBusca) ||
+        dado.descricao.toLowerCase().includes(termoBusca)
+    );
+
+    renderizarCards(dadosFiltrados);
+}
+
+function renderizarCards(dados) {
+    cardContainer.innerHTML = "";
+
+    if (dados.length === 0) {
+        cardContainer.innerHTML = "<p>Nenhum resultado encontrado.</p>";
+        return;
+    }
+
+    for (let dado of dados) {
+        let article = document.createElement('article');
+        article.classList.add('card');
+        article.innerHTML = `
+            <h2>${dado.nome}</h2>
+            <p><strong>Ano:</strong> ${dado.ano}</p>
+            <p>${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">ouvir</a>
+        `;
+        cardContainer.appendChild(article);
+    }
+}
+
+campoBusca.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        iniciarBusca();
+    }
+});
